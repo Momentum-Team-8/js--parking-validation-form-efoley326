b2bdd9f4b460b2721e3c93c7d51bfdd78de5bcc5
Emily Foley <efoley326@gmail.com>
2021-05-21 10:43:12 -0400

    date progress

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 console.log('Add validation!');
+
+function validateNumberInput(numbers)
+if (numberInput.value === "") {
+    alert("enter only numbers");
+    document.numberInput.focus();
+    return false;
+}
+if (!/^[a-zA-Z]*$/.(document.numberInput.value)) {
+    alert("enter only numbers");
+    document.numberInput.focus();
+    return false;
+}
+function caryearInput(numbers)
+
+if (caryear.value < 1900); {
+    alert("choose value larger than 1900")
+    document.caryear.focus();
+    return false;
+}
+
+datedays.value = ""
+if (datedays.value === "") {
+    function validateNumberInput(numbers)
+    }
+
+
+     
+    
+
+
